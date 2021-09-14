cfa2d7ecf42bf56ba3a7fcff5608a6c1948d1a55
goelabhishek694 <goelabhishek694@gmail.com>
2021-09-14 20:04:58 +0530

    restful api done

--- resource.md
+++ resource.md
@@ -1,3 +1,3 @@
 https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 
-
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 
 let user={};
 // client <- server
+//crud- create read update delete
+//read
 app.get('/',(req,res)=>{
     res.send('Home Page');
 });
@@ -20,12 +22,13 @@ app.get('/user',(req,res)=>{
 
 //post request
 // client-> server 
+//create
 app.post('/user',(req,res)=>{
     user=req.body;
     // console.log(req.body);
     res.send('data has been added succesfully');
 });
-
+//update
 app.patch('/user',(req,res)=>{
     let obj=req.body;
     for(let key in obj){
@@ -33,9 +36,18 @@ app.patch('/user',(req,res)=>{
     }
     res.json(user);
 });
-
+//delete 
 app.delete('/user',(req,res)=>{
     user={};
     res.json(user);
+    // res.send('ussr has been deleted');
 })
+//param route
+app.get('/user/:id',(req,res)=>{
+    console.log(req.params);
+    res.json(req.params.id);
+});
+
+
+
 
