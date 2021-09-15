c73fbb2699c6b5dc920988a5b19a3bb8d8ceb062
goelabhishek694 <goelabhishek694@gmail.com>
2021-09-15 20:37:20 +0530

    forgetPassword middleware done

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.listen('5000',function(){
 });
 
 app.use(express.json());
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('i am a middleware');
+//     next();
+// });
+
 app.use(express.static('public'));
 const userRouter=express.Router();
 const authRouter=express.Router();
@@ -14,12 +20,20 @@ const authRouter=express.Router();
 app.use('/user',userRouter);
 app.use('/auth',authRouter);
 //mounting in express
+
+
 userRouter
 .route('/')
 .get(getUser)
 .post(createUser)
 .patch(updateUser)
 .delete(deleteUser);
+
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('i am a middleware 2nd time');
+//     next();
+// });
 
 userRouter
 .route('/:id')
@@ -29,7 +43,40 @@ authRouter
 .route('/signup')
 .post(signupUser);
 
+authRouter
+.route('/forgetPassword')
+.get(getForgetPassword)
+.post(postForgetPassword,validateEmail);
 
+function getForgetPassword(req,res){
+    res.sendFile('./public/forgetPassword.html',{root:__dirname});
+}
+
+function postForgetPassword(req,res,next){
+    let data=req.body;
+    console.log('data',data);
+    //check if email id is correct- validate
+    next();
+    //check if user exists in db
+    // res.json({
+    //     message:"data received",
+    //     data:data.email
+    // })
+};
+
+function validateEmail(req,res){
+    console.log('in validateEmail function');
+    console.log(req.body);
+    //hw to check if email is correct or not -> @ , .
+    //indexOf
+     res.json({
+            message:"data received",
+            data:req.body
+        });
+}
+
+
+https://classroom.pepcoding.com/index
 //redirects
 app.get('/user-all',(req,res)=>{
     res.redirect('/user');
@@ -62,13 +109,14 @@ let user=[];
 // client <- server
 //crud- create read update delete
 //read
-app.get('/',(req,res)=>{
-    res.send('Home Page');
-});
+// app.get('/',(req,res)=>{
+//     res.send('Home Page');
+// });
 
 // app.get('/user',getUser);
 
 function getUser(req,res){
+    console.log('getUser called');
     res.json(user);
 }
 
