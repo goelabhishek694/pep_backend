9b5a6bf8777a118684bb3234b1031b7d206b1e52
goelabhishek694 <goelabhishek694@gmail.com>
2021-09-20 20:02:26 +0530

    added upto mongodb

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const db_link=require('../secrets');
+const {db_link}=require('../secrets.js')
 const validator = require("email-validator");
 mongoose.connect(db_link).then(function(db){
     // console.log(db);
@@ -46,7 +46,7 @@ const userModel=mongoose.model('userModel',userSchema);
     let user={
         name:'Abhi',
         age:20,
-        // email:'abc@gmail.com',
+        email:'abcd@gmail.com',
         password:'12345678',
         confirmPassword:'12345678'
     };
